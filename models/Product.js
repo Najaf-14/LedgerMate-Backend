5ab915e12422c3235a10a8d5148375a1187a4e55
Najaf-14 <najafimran314@gmail.com>
2026-07-01 15:49:57 +0500

    Product schema created

---
+++ models/Product.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+    name: {
+      type: String,
+      require: [true, "Please provide a name for this product."],
+      trim: true,
+      minlength: 3,
+      maxlength: 100,
+    },
+    category: {
+      type: String,
+      trim: true,
+      default: "General",
+    },
+    description: {
+      type: String,
+      trim: true,
+      default: "",
+      maxlength: 500,
+    },
+    purchasePrice: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+    sellingPrice: {
+      type: Number,
+      required: [true, "Selling price is required"],
+      min: 0,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      default: 0,
+      min: 0,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
