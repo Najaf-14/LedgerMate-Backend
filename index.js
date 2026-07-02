9ed8893e87d0e80706cf795d0028aba55bb1c331
Najaf-14 <najafimran314@gmail.com>
2026-07-02 13:06:12 +0500

    Changes

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const cors = require("cors");
 //! Routes
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
-const productRoutes = require("./routes/productRoutes");
 
 app.use(cors());
 app.use(express.json());
@@ -16,7 +15,6 @@ app.use(express.urlencoded());
 connectDB();
 
 app.use("/api/auth", authRoutes);
-app.use("/api/product", productRoutes);
 
 const PORT = process.env.PORT;
 app.listen(PORT, (req, res) => {
