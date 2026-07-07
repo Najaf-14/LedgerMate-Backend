0c83a5adc4c7cd83fc183d11a557bbc4d7971baf
Najaf-14 <najafimran314@gmail.com>
2026-07-07 11:44:34 +0500

    Customer schema created

---
+++ models/Customer.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const customerSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Customer is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      match: [
+        /^[^\s@]+@[^\s@]+\.[a-zA-Z]{2,}$/,
+        "Please enter a valid email address",
+      ],
+    },
+    phoneNo: {
+      type: String,
+      required: [true, "Phone number is required"],
+      unique: true,
+      trim: true,
+      match: [/^03\d{9}$/, "Phone number must be in format 03XXXXXXXXX"],
+    },
+    address: {
+      type: String,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Customer = mongoose.model("customer", customerSchema);
