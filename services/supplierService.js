f063c665516660a2f1693c431823e662576d8f73
Najaf-14 <najafimran314@gmail.com>
2026-07-09 10:57:43 +0500

    Supplier logic

---
+++ services/supplierService.js
@@ -0,0 +1,96 @@
+const Supplier = require("../models/Supplier");
+
+const createSupplier = async (data, businessId) => {
+  const supplierExists = await Supplier.findOne({
+    business: businessId,
+    phoneNo: data.phoneNo,
+  });
+
+  if (supplierExists) {
+    const error = new Error("Supplier already exists");
+    error.statusCode = 400;
+    throw error;
+  }
+
+  return await Supplier.create({
+    ...data,
+    business: businessId,
+  });
+};
+
+const getSuppliers = async (businessId) => {
+  return await Supplier.find({
+    business: businessId,
+  }).sort({ createdAt: -1 });
+};
+
+const searchSuppliers = async (query, businessId) => {
+  return await Supplier.find({
+    business: businessId,
+    name: {
+      $regex: query,
+      $options: "i",
+    },
+  }).sort({ createdAt: -1 });
+};
+
+const getSupplier = async (id, businessId) => {
+  const supplier = await Supplier.findOne({
+    _id: id,
+    business: businessId,
+  });
+
+  if (!supplier) {
+    const error = new Error("Supplier not found");
+    error.statusCode = 404;
+    throw error;
+  }
+
+  return customer;
+};
+
+const updateSupplier = async (id, data, businessId) => {
+  const supplier = await Customer.findOneAndUpdate(
+    {
+      _id: id,
+      business: businessId,
+    },
+    data,
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
+
+  if (!supplier) {
+    const error = new Error("Customer not found");
+    error.statusCode = 404;
+    throw error;
+  }
+
+  return supplier;
+};
+
+const deleteSupplier = async (id, businessId) => {
+  const supplier = await Supplier.findOne({
+    _id: id,
+    business: businessId,
+  });
+
+  if (!supplier) {
+    const error = new Error("Supplier not found");
+    error.statusCode = 404;
+    throw error;
+  }
+
+  await customer.deleteOne();
+};
+
+module.exports = {
+  createSupplier,
+  getSuppliers,
+  getSupplier,
+  searchSuppliers,
+  updateSupplier,
+  deleteSupplier,
+};
