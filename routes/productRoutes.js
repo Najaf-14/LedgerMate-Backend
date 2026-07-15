178917f6199e14c676c9adb745ac5109eb92e5a5
Najaf-14 <najafimran314@gmail.com>
2026-07-15 16:14:25 +0500

    product routes

---
+++ routes/productRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const authMiddleware = require("../middleware/authMiddleware");
+const router = express.Router();
+const {
+  createProduct,
+  getProducts,
+  searchProducts,
+  getProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/poductController");
+
+router.use(authMiddleware);
+
+router.post("/", createProduct);
+router.get("/", getProducts);
+router.get("/search", searchProducts);
+router.get("/:id", getProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+
+module.exports = router;
