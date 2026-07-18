6abed76a851ce6fdb3b1323a58e128359d999e27
Najaf-14 <najafimran314@gmail.com>
2026-07-18 20:07:46 +0500

    routes correction

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { model } = require("mongoose");
 const router = express.Router();
-const authMiddleware = require("../middleware/authMiddleware")
+const authMiddleware = require("../middleware/authMiddleware");
 
 const { signup, login, getMe } = require("../controllers/authController");
 
