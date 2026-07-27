c4e314a159cb1921c0ec226e1f2812337225114b
Najaf-14 <najafimran314@gmail.com>
2026-07-27 16:20:19 +0500

    server seperation

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const app = require("./index");
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
