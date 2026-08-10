1fecca816b6a910a6a1ed31e1a675827c9f693ca
Najaf-14 <najafimran314@gmail.com>
2026-08-10 16:22:18 +0500

    server changes

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = require("./index");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
