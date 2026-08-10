f56f3118a80f98b75a362006e3dafe5f2be90f49
Najaf-14 <najafimran314@gmail.com>
2026-08-10 16:14:59 +0500

    firebase push notification

---
+++ config/firebase.js
@@ -0,0 +1,11 @@
+const { initializeApp, cert } = require("firebase-admin/app");
+
+const firebaseApp = initializeApp({
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
+});
+
+module.exports = firebaseApp;
