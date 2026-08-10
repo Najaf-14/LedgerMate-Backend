9c12543ac023d88de61aef2c0acac637c71da070
Najaf-14 <najafimran314@gmail.com>
2026-08-10 16:20:36 +0500

    push notification

---
+++ services/notificationService.js
@@ -0,0 +1,30 @@
+const { getMessaging } = require("firebase-admin/messaging");
+
+const sendNotification = async (token, title, body, data = {}) => {
+  try {
+    const message = {
+      token,
+
+      notification: {
+        title,
+        body,
+      },
+
+      data,
+    };
+
+    const response = await getMessaging().send(message);
+
+    console.log("Notification sent:", response);
+
+    return response;
+  } catch (error) {
+    console.error("Notification error:", error);
+
+    throw error;
+  }
+};
+
+module.exports = {
+  sendNotification,
+};
