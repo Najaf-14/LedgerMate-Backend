157224aab1be241df6b1147b5adcb07ae097ca75
Najaf-14 <najafimran314@gmail.com>
2026-08-11 15:46:39 +0500

    added payment supplier

--- models/Payment.js
+++ models/Payment.js
@@ -10,7 +10,10 @@ const paymentSchema = new mongoose.Schema(
     customer: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Customer",
-      required: true,
+    },
+    supplier: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Supplier",
     },
     amount: {
       type: Number,
