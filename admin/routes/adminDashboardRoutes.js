cb645c4d3fb192c7080feca9cac5b7cda51c8be2
Najaf-14 <najafimran314@gmail.com>
2026-08-20 00:33:41 +0500

    admin

---
+++ admin/routes/adminDashboardRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const router = express.Router();
+
+const authMiddleware = require("../../middleware/authMiddleware");
+const adminMiddleware = require("../../middleware/adminMiddleware");
+
+const {
+  getDashboardStats,
+} = require("../controllers/adminDashboardController");
+
+router.get("/stats", authMiddleware, adminMiddleware, getDashboardStats);
+
+module.exports = router;
